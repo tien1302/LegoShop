ee99a334e4733e7807bc8edb5d1d3e1f52884a26
tien1302 <tiennqse150168@fpt.edu.vn>
2023-06-19 19:27:29 +0700

    client home login

---
+++ LegoShop/WebClient/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace WebClient.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
